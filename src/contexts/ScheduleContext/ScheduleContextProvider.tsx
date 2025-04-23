9b781cf4c0bb289c43a94c298fbfb3a0c0eaab8c
Jordano Ypsum <ypsum@outlook.com>
2025-04-23 16:41:34 -0300

    WIP - Persistência com local storage implementada para schedule;

--- src/contexts/ScheduleContext/ScheduleContextProvider.tsx
+++ src/contexts/ScheduleContext/ScheduleContextProvider.tsx
@@ -1,7 +1,8 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { scheduleReducer } from './scheduleReducer';
 import { initialScheduleState } from './initialScheduleState';
 import { ScheduleContext } from './ScheduleContext';
+import { ScheduleStateModel } from '../../models/ScheduleStateModel';
 
 type ScheduleContextProviderProps = {
   children: React.ReactNode;
@@ -10,7 +11,25 @@ type ScheduleContextProviderProps = {
 export function ScheduleContextProvider({
   children,
 }: ScheduleContextProviderProps) {
-  const [state, dispatch] = useReducer(scheduleReducer, initialScheduleState);
+  const [state, dispatch] = useReducer(
+    scheduleReducer,
+    initialScheduleState,
+    () => {
+      const storageState = localStorage.getItem('state');
+
+      if (!storageState) return initialScheduleState;
+
+      const parsedStorageState = JSON.parse(storageState) as ScheduleStateModel;
+
+      return {
+        ...parsedStorageState,
+      };
+    },
+  );
+
+  useEffect(() => {
+    localStorage.setItem('state', JSON.stringify(state));
+  }, [state]);
 
   return (
     <ScheduleContext.Provider value={{ state, dispatch }}>
